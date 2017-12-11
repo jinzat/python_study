8458b8137f55a761c708d31844f81ce2bf1da252
jinzat <jinzat@outlook.com>
2017-12-12 00:41:41 +0800

    prime number

--- lesson 2/prime.py
+++ lesson 2/prime.py
@@ -1,14 +1,11 @@
 '''count prime number'''
-while i in range (1,51):
-    
-    a=(2**i-2)/i
-    b=a//1
-    c=i
-    
-    if (a==b):
-        print(c)
-
-        for m in range (51,101):
-            if m%c!=0:
-                print (m)
+for i in range (2,101):
+    k=0
+    for j in range (2,i-1):
+        if i%j==0:
+            k=1
+           
+        elif k==0:
+            print(i)
+   
     
