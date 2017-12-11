e9d8fbf1023463a44613a6252f3fec22c5a00c07
jinzat <jinzat@outlook.com>
2017-12-11 23:09:57 +0800

    update peibonaqie

--- lesson 2/peibonaqie.py
+++ lesson 2/peibonaqie.py
@@ -2,12 +2,14 @@
 a=0
 b=1 # a 为 0 b 为 1
 i=1
-
+c=0
 while i in range (0,101):
-    print(b) #然后进行循环
-    a=b 
+    print('a',a,'b',b)
+    c=b
     b=a+b
+    a=c
     i=i+1
+
     if (i==99): #输出第99项
        c=b
     if (i==100):#输出第100项
